6a5461a7508a6ea2a9062444e1fd98e6e4bf2ca5
Nazzcodek <bellnas09@gmail.com>
2024-02-01 12:39:39 +0000

    task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""MRU module"""
+from base_caching import BaseCaching as BC
+
+
+class MRUCache(BC):
+    '''this is mru caching object instance'''
+
+    def __init__(self):
+        super().__init__()
+        self.use_order = []
+
+    def put(self, key, item):
+        '''this is a cache put method'''
+        if key is None or item is None:
+            return
+
+        data = self.cache_data
+        order = self.use_order
+
+        if len(data) >= BC.MAX_ITEMS:
+            if order:
+                discard = order.pop(0)
+                del data[discard]
+                print(f'DISCARD: {discard}')
+
+        data[key] = item
+        order.append(key)
+
+    def get(self, key):
+        '''this method get cache key'''
+        if key in self.cache_data:
+            self.use_order.remove(key)
+            self.use_order.append(key)
+
+        return self.cache_data.get(key, None)
