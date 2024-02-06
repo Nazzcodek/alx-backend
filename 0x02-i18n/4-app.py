1a5e863b7b17be5907bee2b621c89b4a1280cb69
Nazzcodek <bellnas09@gmail.com>
2024-02-06 11:06:01 +0000

    task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -21,11 +21,11 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale() -> str:
     """this is the get locale method"""
-    args = request.args
-    if 'locale' in args and args['locale'] in app.config['LANGUAGES']:
-        return request.args['locale']
-    else:
-        return request.accept_languages.best_match(app.config['LANGUAGES'])
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/',  methods=['GET'], strict_slashes=False)
